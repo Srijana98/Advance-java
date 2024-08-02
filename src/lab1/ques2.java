8a64a7d296e70681086c57762f44b00edfdd5cb1
Srijana98 <srijana.neupane897@gmail.com>
2024-08-02 11:12:10 +0545

    Update ques2.java

--- src/lab1/ques2.java
+++ src/lab1/ques2.java
@@ -1,5 +1,41 @@
 package lab1;
+class ques2 {
+    private String customerName;
+    private String size;
+    private int numberOfToppings;
 
-public class ques2 {
+    // Constructor
+    public ques2(String customerName, String size, int numberOfToppings) {
+        this.customerName = customerName;
+        this.size = size;
+        this.numberOfToppings = numberOfToppings;
+    }
 
+    // Getters and Setters
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public int getNumberOfToppings() {
+        return numberOfToppings;
+    }
+
+    public void setNumberOfToppings(int numberOfToppings) {
+        this.numberOfToppings = numberOfToppings;
+    }
 }
+
+
+
